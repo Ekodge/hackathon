febd5bb5ccf8e5fe70f77883204b36be737aae48
Ekodge <Ethan.cadorel@hotmail.fr>
2025-01-10 12:54:51 +0100

    migration shop for 'dist'

---
+++ pb_migrations/1736510024_updated_shop.js
@@ -0,0 +1,27 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2338794258")
+
+  // remove field
+  collection.fields.removeById("number2172440823")
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2338794258")
+
+  // add field
+  collection.fields.addAt(11, new Field({
+    "hidden": false,
+    "id": "number2172440823",
+    "max": null,
+    "min": null,
+    "name": "dist",
+    "onlyInt": false,
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "number"
+  }))
+
+  return app.save(collection)
+})
